2c2f25fd288bb3c919ef7dcf02def7a6dda3c4df
seanmullane <mullane.s.p@gmail.com>
2018-12-14 01:49:23 -0500

    Added plots of cap and helix length and fit regression

--- poisson_fits.R
+++ poisson_fits.R
@@ -68,3 +68,42 @@ ggplot() +
 
 # It still doesn't seem to be a good fit
 
+
+# Try to fit a Poisson model to helix lengths - this should be a good fit
+
+slens <- read.csv(file = "sequence_lengths.csv")
+
+pmodel4 <- glm(len_cnt ~ lengthSEQ, family = "poisson", data = slens)
+summary(pmodel4)
+
+fitted2 <- fitted(pmodel4)
+qplot(x = slens$lengthSEQ, fitted2)
+ggplot() +
+  geom_point(mapping = aes(x = slens$lengthSEQ, y = slens$len_cnt)) +
+  geom_path(mapping = aes(x = slens$lengthSEQ, y = fitted2)) +
+  ggtitle("Helix+Cap Length vs Poisson Fitted Counts")
+
+ggplot() +
+  geom_point(mapping = aes(x = slens$lengthSEQ, y = resid(pmodel4))) +
+  geom_hline(yintercept = 0) +
+  ggtitle("Helix+Cap Length vs Poisson Deviance Residuals")
+
+
+
+# Make nice plot for data pipeline presentation
+ggplot() +
+  theme_bw() +
+  geom_point(mapping = aes(x = hlens$lengthHELIX, y = hlens$len_cnt)) +
+  geom_vline(xintercept = 4.5, colour = "red") +
+  labs(x = "Helix Length (Residues)", y = "n") +
+  ggtitle("Helix Length vs Count in CAPSDB")
+
+typelens <- read.csv(file = "typelen.csv")
+typelens$type <- as.factor(typelens$type)
+
+ggplot() +
+  theme_bw() +
+  scale_color_manual(values=c("#d56e2a", "#2478b4")) +
+  geom_point(mapping = aes(x = lengthCAP, y = count, color = type), data = typelens) +#, stat = "identity", position = "dodge") +
+  labs(x = "Cap Length (Residues)", y = "n") +
+  ggtitle("Helix Cap Length vs Count and Type in CAPSDB")
